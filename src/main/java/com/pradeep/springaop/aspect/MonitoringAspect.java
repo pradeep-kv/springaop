c5c2b89e80e0e03bb5e88ccd144e155181074644
pradeep-kv <pradeepkv1324@gmail.com>
2020-12-21 18:14:20 +0530

    Added Joinpoint to get method signature and arguments.

--- src/main/java/com/pradeep/springaop/aspect/MonitoringAspect.java
+++ src/main/java/com/pradeep/springaop/aspect/MonitoringAspect.java
@@ -1,8 +1,11 @@
 package com.pradeep.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +15,15 @@ import org.springframework.stereotype.Component;
 public class MonitoringAspect {
 
     @Before(value = "com.pradeep.springaop.aspect.LogAspect.exePackageWildcard()")
-    public void beforeTest(){
-        System.out.println("Test Before MonitoringAspect AOP working with order");
+    public void beforeTest(JoinPoint joinPoint){
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Test Before MonitoringAspect AOP working with order for method : \n" + signature);
+
+        Object[] argObj = joinPoint.getArgs();
+        System.out.println("Method Args are ::");
+        for (Object obj: argObj) {
+            System.out.println(obj);
+        }
     }
 
     @After(value = "com.pradeep.springaop.aspect.LogAspect.exePackageServiceWildcard()")
