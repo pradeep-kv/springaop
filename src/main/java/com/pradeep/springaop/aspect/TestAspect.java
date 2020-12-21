39e0b0b96b4bd772281bffb6a52a25a239a76e1a
pradeep-kv <pradeepkv1324@gmail.com>
2020-12-21 12:25:45 +0530

    Added Aspect Pointcut expression.

--- src/main/java/com/pradeep/springaop/aspect/TestAspect.java
+++ src/main/java/com/pradeep/springaop/aspect/TestAspect.java
@@ -3,18 +3,25 @@ package com.pradeep.springaop.aspect;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Aspect
 public class TestAspect {
 
-    @Before(value = "execution (* com.pradeep..*())")
+    @Pointcut("execution (* com.pradeep..*())")
+    private void exePackageWildcard(){}
+
+    @Pointcut("execution(* com.pradeep.springaop.service..*(..)))")
+    private void exePackageServiceWildcard(){}
+
+    @Before(value = "exePackageWildcard()")
     public void beforeTest(){
         System.out.println("Test Before AOP working");
     }
 
-    @After(value = "execution (* com.pradeep.springaop.service..*())")
+    @After(value = "exePackageServiceWildcard()")
     public void Aftertest(){
         System.out.println("Test After AOP working");
     }
